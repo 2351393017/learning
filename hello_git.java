0b53717aa1f85bbcc810a665f7ade1515fae23a1
sinfunction <2351393017@qq.com>
2021-11-08 17:27:34 +0800

    上传文件

---
+++ hello_git.java
@@ -0,0 +1,12 @@
+package java_learning;
+
+public class hello_git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello git");
+		
+	}
+
+}
